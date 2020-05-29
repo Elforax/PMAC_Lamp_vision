5240f02b68c4b68eeb96c34d0485afd07bfe8437
Elforax <justingort2@gmail.com>
2020-05-29 18:34:26 +0200

    added new py file

--- Python/Lamp_detect.py
+++ Python/Lamp_detect.py
@@ -5,15 +5,25 @@ import sys
 
 # PMAC is een lib waar functies instaan die voor alle test scripts gebruikt kunnen worden
 # import de gene die je nodig hebt
-from PMAC import scale_img
+from PMAC import scale_img, image_set
 
 print("You are using OpenCV version " + cv2.__version__ + ".")
 
 if __name__ == "__main__":
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-    image = cv2.imread("pictures/lamp schoon.jpg")
-    image = scale_img(image, 0.2)
+    paths, names = image_set("pictures")
+    print(paths, names)
 
-    cv2.imshow("Original", image)
+    images = []
+    for i in range(0, len(paths), 1):
+        input_img = cv2.imread(paths[i])
+        input_img = scale_img(input_img, 0.2)
+        images.append(input_img)
+
+    i = 0
+    for image in images:
+        cv2.imshow(names[i], image)
+        i += 1
+
     cv2.waitKey(0)
--- Python/PMAC.py
+++ Python/PMAC.py
@@ -2,15 +2,31 @@ import numpy as np
 import cv2
 import os
 import sys
+import glob
 
 # PMAC is een lib waar functies instaan die voor alle test scripts gebruikt kunnen worden
 # import de gene die je nodig hebt
 
 
 def scale_img(img, scale=1.0):
-    scale_v = round(len(img) * scale)
-    scale_u = round(len(img[0]) * scale)
+    _scale_v = round(len(img) * scale)
+    _scale_u = round(len(img[0]) * scale)
 
-    print(scale_u, "x" ,scale_v)
-    img = cv2.resize(img, (scale_u, scale_v))
-    return img
+    print(_scale_u, "x", _scale_v)
+    _img = cv2.resize(img, (_scale_u, _scale_v))
+    return _img
+
+
+def image_set(path, file_type="jpg"):    # points to a folder and takes all the images in it.
+    _data = glob.glob(path + "/*." + file_type)
+
+    _paths = []
+    _names = []
+    for file in _data:
+        _split_data = file.split("\\")
+
+        _file_path = path + "/" + _split_data[len(_split_data)-1]
+
+        _names.append(_split_data[len(_split_data)-1])
+        _paths.append(_file_path)
+    return _paths, _names
